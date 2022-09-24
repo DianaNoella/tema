740d329355b23ff1fa4bc1b69fb553d96384ef22
Noella <dianasava18@gmail.com>
2022-09-24 19:06:51 +0300

    tema1

---
+++ data_structures/tema1.py
@@ -0,0 +1,13 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+my_list.sort()
+print(my_list)
+my_list.sort(reverse=True)
+print(my_list)
+my_sliced_list = my_list[::2]
+print(my_sliced_list)
+my_sliced_list2 = my_list[1::2]
+print(my_sliced_list2)
+my_sliced_list3 = my_list[1::3]
+print(my_sliced_list3)
+#sort() inlocuieste elem in lista
+#sorted() creeaza o lista noua
